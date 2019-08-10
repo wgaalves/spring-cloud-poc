282a0d39517521abbb4bdfe50edf81d725443316
william <coyothebr@gmail.com>
2019-08-10 10:18:13 -0300

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+### SPRING CLOUD PROOF OF CONCEPT (POC)
+
+
+### SERVICES
+
+* auth-Service              port 9000
+* config-service            port 8081
+* discovery-service port    port 8082
+* gateway                   port 8000
+* mail-Service              port 8084
+* monitoring-Service        port 8085
+* turbine-stream-service    port 8086
---
+++ monitoring-service/src/main/java/com/willalves/springpoc/monitoringservice/MonitoringServiceApplication.java
@@ -0,0 +1,15 @@
+package com.willalves.springpoc.monitoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceApplication.class, args);
+	}
+
+}
