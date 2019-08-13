a33b07c36c4db25cc8b9f4b8c450dda559ccaa81
William Gabriel de Arruda Alves <williamalves@ext.cvccorp.com.br>
2019-08-13 17:46:38 -0300

    fixes

--- heimdall/src/main/java/com/musketeers/springpoc/heimdall/HeimdallApplication.java
+++ heimdall/src/main/java/com/musketeers/springpoc/heimdall/HeimdallApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-//@EnableOAuth2Client
-@EnableFeignClients
-@EnableCircuitBreaker
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableEurekaClient
 public class HeimdallApplication {
 
 	public static void main(String[] args) {
