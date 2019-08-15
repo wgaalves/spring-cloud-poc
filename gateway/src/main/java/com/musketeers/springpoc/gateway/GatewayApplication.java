2f0a86a508c53c39b58c007a785d59902cb106be
William Gabriel de Arruda Alves <williamalves@ext.cvccorp.com.br>
2019-08-15 18:31:34 -0300

    fixes

--- gateway/src/main/java/com/musketeers/springpoc/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/musketeers/springpoc/gateway/GatewayApplication.java
@@ -1,17 +1,26 @@
 package com.musketeers.springpoc.gateway;
 
+import com.musketeers.springpoc.gateway.pre.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 
 }
